aa4696779bcb7cc516472d6fc5a66e7ce971d1ef
Jeremy Wells <jeremy@boost.co.nz>
2009-04-14 10:14:03 +1200

    Added helper specs

--- spec/lib/radiant_consumer_helper_spec.rb
+++ spec/lib/radiant_consumer_helper_spec.rb
@@ -1,4 +1,28 @@
-# To change this template, choose Tools | Templates
-# and open the template in the editor.
+require File.expand_path(File.join(File.dirname(__FILE__), '../spec_helper'))
 
-puts "Hello World"
+class RadiantConsumerController
+  include RadiantConsumerHelper
+end
+
+describe RadiantConsumerController do
+  before(:each) do
+    @controller = RadiantConsumerController.new
+    @consumer = mock(:radiant_consumer)
+    RadiantConsumer.should_receive(:instance).and_return(@consumer)
+  end
+
+  it 'should fetch a radiant page' do
+    @consumer.should_receive(:fetch_page).with('name')
+    @controller.radiant_page('name')
+  end
+
+  it 'should fetch a radiant page part' do
+    @consumer.should_receive(:fetch_page_part).with('name', 'part')
+    @controller.radiant_page_part('name', 'part')
+  end
+
+  it 'should fetch a radiant snippet' do
+    @consumer.should_receive(:fetch_snippet).with('name')
+    @controller.radiant_snippet('name')
+  end
+end
